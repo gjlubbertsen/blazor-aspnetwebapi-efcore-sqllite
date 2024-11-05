f10cffddeeb19bf6e7b5926983d764e888ff2580
Gerrit-Jan Lubbertsen <gerrit-jan.lubbertsen@afas.nl>
2024-11-05 09:07:29 +0100

    Move to Nunit fluentAssertions and FakeItEasy

---
+++ src/Backend/Contoso.Pizza.Data.Tests/PriceCalculatorServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Contoso.Pizza.AdminApi.Models;
+
+[TestFixture]
+public class PriceCalculatorServiceTests
+{
+    private PriceCalculatorService _sut = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new PriceCalculatorService();
+    }
+ 
+    [Test]
+    public void PriceCalculated()
+    {
+        _sut.CalculatePrice(new PizzaEntity() {
+            Name = "Test Pizza",
+            Price = 0,
+            Sauce = new SauceEntity { Price = 1 },
+            Toppings = new List<ToppingEntity> {
+                new ToppingEntity { Name = "A", Price = 2 },
+                new ToppingEntity { Name = "B", Price = 2 }
+            }
+        }).Should().Be(10);
+    }
+}
+
